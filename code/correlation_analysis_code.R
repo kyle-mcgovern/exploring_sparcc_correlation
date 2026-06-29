39fc615e43c1b9a757355c1bd00f0829b05680be
kyle-mcgovern <kyle.mcgovern93@gmail.com>
2026-06-29 16:28:09 -0700

    more udpates

--- code/correlation_analysis_code.R
+++ code/correlation_analysis_code.R
@@ -1,10 +1,15 @@
 library(abind)
-
+library(rBeta2009)
+set.seed(12754)
 
 #######################
 #### Function Code ####
 #######################
 
+
+closure <- function(X) {
+  return(apply(X, 2, function(col) col/sum(col)))
+}
 
 #' Estimates absolute abundance correlations given relative abundances,
 #' scale variance, and correlations between each taxon and scale.
@@ -35,8 +40,10 @@ absCorrEst <- function(rel_abund, scale_var, scale_rel_corr) {
   return(cov2cor(abs_cov))
 }
 
-bayesianAbsCorr <- function(Y, scale_var_sampler, scale_var_args=list(),
+bayesianAbsCorr <- function(Y, scale_var_sampler, scale_rel_sampler,
+			    scale_var_args=list(), scale_rel_args=list(),
 			    nsample=1000) {
+  taxa_names <- row.names(Y)
   all_abs_corr <- c() 
   for(i in 1:nsample) {
     # Sample scale args
@@ -45,9 +52,11 @@ bayesianAbsCorr <- function(Y, scale_var_sampler, scale_var_args=list(),
     # Dirichlet Sample Relative Abundances
     P_sample <- apply(Y_genus, 2, function(col) rdirichlet(1, col+0.5))
     # Estimate absolute correlations
-    abs_corr <- cov2cor(matrix_cov(P_sample, sigma_perp2, x))
-    all_abs_corr <- abind(all_res, R, along=3)
+    abs_corr <- cov2cor(absCorrEst(P_sample, scale_var, scale_rel_corr))
+    all_abs_corr <- abind(all_abs_corr, abs_corr, along=3)
   }
+  row.names(all_abs_corr) <- taxa_names
+  colnames(all_abs_corr) <- taxa_names
   return(all_abs_corr)
 }
 
@@ -79,11 +88,25 @@ sparcc_basis <- function(P) {
 sparcc <- function(Y, nsample=1000) {
   corr_mats <- c()
   for (iter in 1:nsample) {
-    P_sample <- apply(Y, 2, function(col) rdirichlet(1, col+prior))
+    P_sample <- apply(Y, 2, function(col) rdirichlet(1, col+0.5))
     corr_mat <- sparcc_basis(P_sample)
     corr_mats <- abind(corr_mats, corr_mat, along=3)
   }
   return(corr_mats)
+}
+
+write_df <- function(taxon_i, abs_corr, res) {
+  # Write_dfs
+  i <- which(rownames(abs_corr) == taxon_i)
+  post <- t(res[i, , ])  # samples x taxa
+  df <- data.frame(
+    taxon = row.names(abs_corr),
+    median = apply(post, 2, median),
+    lower = apply(post, 2, quantile, 0.025),
+    upper = apply(post, 2, quantile, 0.975),
+    true = abs_corr[taxon_i, ]
+  )
+  return(df)
 }
 
 #######################
@@ -110,6 +133,36 @@ Y_genus <- rbind(Y_genus, other)
 Y_genus <- Y_genus[,colnames(Y_genus)%in%row.names(metadata)]
 metadata <- metadata[colnames(Y_genus),]
 
+## Absolute correlations
+rel_abund <- closure(Y_genus + 0.5)
+abs_abund <- sweep(rel_abund, 2, metadata$Average.cell.count..per.gram.of.frozen.feces., FUN="*")
+abs_corr <- cor(t(log(abs_abund)))
+
 ## SparCC Analysis
 sparcc_corr_res <- sparcc(Y_genus, nsample=2000)
-print(sparcc_corr_res)
+
+# Write_dfs
+df <- write_df("Oscillibacter", abs_corr, sparcc_corr_res)
+write.csv(df, "../results/sparcc_oscillibacter.csv", row.names=F)
+df <- write_df("Prevotella", abs_corr, sparcc_corr_res)
+write.csv(df, "../results/sparcc_prevotella.csv", row.names=F)
+
+## Bayesian Sampling Correlation Analysis
+scale_var_sampler <- function(n, s_l, s_u) {
+  return(runif(n, s_l, s_u))
+}
+scale_rel_sampler <- function(n, D, m_l, m_u, r_l, r_u) {
+  m <- runif(n, m_l, m_u)
+  scale_rel_corr <- runif(D, m+r_l, m+r_u)
+  return(scale_rel_corr)
+}
+scale_var_args <- list(s_l=0.4, 0.8)
+scale_rel_args <- list(m_l=0, m_u=0.15, r_l=-0.3, r_u=0.3, D=nrow(abs_abund))
+bayes_corr_res <- bayesianAbsCorr(Y_genus, scale_var_sampler,
+				  scale_rel_sampler, scale_var_args,
+				  scale_rel_args, nsample=2000)
+
+df <- write_df("Oscillibacter", abs_corr, bayes_corr_res)
+write.csv(df, "../results/bayes_oscillibacter.csv", row.names=F)
+df <- write_df("Prevotella", abs_corr, bayes_corr_res)
+write.csv(df, "../results/bayes_prevotella.csv", row.names=F)
